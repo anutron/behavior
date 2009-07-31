23efc246a7688c57ee3fde93b58b14d54101fd6c
Aaron Newton <git@clientcide.com>
2009-07-31 11:21:18 -0700

    adding submodules for all dependencies so tests can run adding submodule for tests adding tests for window

---
+++ Tests/UserTests/Widgets/ART.Widget.Window.all.js
@@ -0,0 +1,84 @@
+{
+	tests: [
+		{
+			title: "Makes a Window",
+			description: "Makes a window that you can drag around and stuff.",
+			verify: "Can you move the window around and resize it?",
+			before: function(){
+				
+				ART.Sheet.defineStyle('window', {
+					'height': 200,
+					'width': 250,
+
+					'max-height': 440,
+					'max-width': 390,
+
+					'min-height': 100,
+					'min-width': 200,
+				});
+				
+				var w = new ART.Widget.Window({caption: 'This is the caption'});
+
+				$(w).inject('container');
+
+				$(w).setStyles({'top': 40, 'left': 0});
+
+				w.setContent($$('.some-content')[0].clone());
+
+				w.addEvent('close', function(){
+					w.destroy();
+				});
+
+				w.addEvent('minimize', function(){
+					w.resize(100, 100);
+				});
+
+				w.addEvent('maximize', function(){
+					w.resize(250, 300);
+				});
+			}
+		},
+		{
+			title: "Makes a group of Windows",
+			description: "Makes numerous windows; you should be able to focus them to bring them to the top.",
+			verify: "Can you interact with the windows? When you click one does it focus and come to the top of the other windows?",
+			before: function(){
+				
+				ART.Sheet.defineStyle('window', {
+					'height': 200,
+					'width': 250,
+
+					'max-height': 440,
+					'max-width': 390,
+
+					'min-height': 100,
+					'min-width': 200,
+				});
+				var makeWindow = function() {
+					var w = new ART.Widget.Window({caption: 'This is the caption'});
+
+					$(w).inject('container');
+
+					$(w).setStyles({'top': 40, 'left': 0});
+
+					w.setContent($$('.some-content')[0].clone());
+
+					w.addEvent('close', function(){
+						w.destroy();
+					});
+
+					w.addEvent('minimize', function(){
+						w.resize(100, 100);
+					});
+
+					w.addEvent('maximize', function(){
+						w.resize(250, 300);
+					});
+				};
+				(5).times(makeWindow);
+				ART.WM.cascade(true, 0, 40);
+			}
+		}
+	],
+	otherScripts: ['touch', 'MgOpen.Moderna', 'MgOpen.Moderna.Bold', 'Selectors']
+}
