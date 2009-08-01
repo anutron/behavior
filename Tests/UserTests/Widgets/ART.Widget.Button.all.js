08d3fe8b08159acc70b260577f28695a78877137
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-07-31 22:27:27 -0400

    NEW Test for "ART.Widget.Button"

---
+++ Tests/UserTests/Widgets/ART.Widget.Button.all.js
@@ -0,0 +1,14 @@
+{
+	tests: [
+		{
+			title: "Makes a Button",
+			description: "Makes a button that you can click and stuff.",
+			verify: "Does the button exist?",
+			before: function(){
+				var b = new ART.Widget.Button({label: 'Hello, I am a button!'});
+				$(b).inject('container');
+			}
+		}
+	],
+	otherScripts: ['touch', 'MgOpen.Moderna', 'MgOpen.Moderna.Bold', 'Selectors']
+}
