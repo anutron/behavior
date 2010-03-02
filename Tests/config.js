3418fea72694da2c41201e4846fc15fa1d6a78cd
Aaron Newton <git@clientcide.com>
2010-03-02 14:53:07 -0800

    updating git modules; dependencies

--- Tests/config.js
+++ Tests/config.js
@@ -4,16 +4,19 @@ var UnitTester = {
 	path: 'UnitTester/',
 	ready: function(){
 		var sources = {
-			core: '../assets/core',
-			more: '../assets/more',
-			color: '../assets/color',
-			touch: '../assets/touch',
-			slick: '../assets/slick',
-			art: '../'
+			core: '../assets/core/Source/scripts.json',
+			more: '../assets/more/Source/scripts.json',
+			color: '../assets/mootools-color/scripts.json',
+			table: '../assets/mootools-table/scripts.json',
+			touch: '../assets/touch/Source/scripts.json',
+			slick: '../assets/slick/Source/scripts.json',
+			art: '../assets/art/scripts.json',
+			widgets: '../Source/scripts.json'
 		};
 		window.ut = new UnitTester(sources, {
 			'art': 'UserTests'
 		}, {
+			appendSource: false,
 			autoplay: true
 		});
 	}
