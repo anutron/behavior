22bde6763aed397a5b15e0f085ec97df15c3584d
Aaron Newton <git@clientcide.com>
2011-01-12 08:55:22 -0800

    fixing the passed method references to applyFilter

--- Source/Behavior.js
+++ Source/Behavior.js
@@ -40,10 +40,9 @@ provides: [DashSelectors, Behavior]
 				removeEvent: this.removeEvent.bind(this),
 				addEvents: this.addEvents.bind(this), 
 				removeEvents: this.removeEvents.bind(this),
-				applyBehaviors: this.apply.bind(this),
-				applyBehavior: this.applyBehavior.bind(this),
-				//Weird, but is the only real way to allow communication between methods
 				fireEvent: this.fireEvent.bind(this),
+				applyFilters: this.apply.bind(this),
+				applyFilter: this.applyFilter.bind(this),
 				//this doesn't really stand up; the container is variable - one behavior instance
 				//can handle numerous containers... TODO: revisit
 				getContainerSize: function() { return this.currentSize; }.bind(this),
