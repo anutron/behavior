40d4f62ebd68c1df5bc77bf6c80e6d3246033514
Aaron Newton <git@clientcide.com>
2011-03-30 14:52:44 -0700

    closing my benchmark's div

--- Specs/Behavior/Behavior.Benchmarks.js
+++ Specs/Behavior/Behavior.Benchmarks.js
@@ -1,5 +1,5 @@
 (function(){
 	Behavior.addGlobalFilter('Test1', function(){});
 
-	MooBench.addBehaviorTest('Behavior: Empty filters', '<div data-filters="Test1">');
+	MooBench.addBehaviorTest('Behavior: Empty filters', '<div data-filters="Test1"></div>');
 })();
