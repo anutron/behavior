ff6602bc75eff381149ba62723f864515e5ac1d6
Aaron Newton <git@clientcide.com>
2011-11-04 18:24:27 -0700

    a minor refactor for cleanliness.

--- Source/Behavior.Startup.js
+++ Source/Behavior.Startup.js
@@ -6,32 +6,35 @@ requires: [/Behavior, /Delegator]
 provides: [Behavior.Startup]
 ...
 */
-
-Behavior.addGlobalFilter('Startup', {
-	setup: function(el, api){
-		//get the delegators to set up
-		var delegators = api.get('delegators');
-		if (delegators) {
-			Object.each(delegators, function(conditional, delegator){
-				var targets = [];
-				//get the target of these delegators
-				if (api.get('targets')){
-					targets = el.getElement(api.get('targets'));
-				} else if (api.get('target') && api.get('target') != 'self'){
-					var target = el.getElement(api.get('target'));
-					if (target) targets = new Elements([target]);
-				} else {
-					targets = new Elements(el);
-				}
-				if (targets.length == 0) api.fail('could not find targets for startup delegator: ', delegator, api.get('targets'));
-				//check the targets for the conditionals
-				var fire = targets.some(function(target){
-					if (conditional.property) return element.get(conditional.property) === conditional.value;
-					else if (conditional.method) return element[method].apply(element, conditional.arguments || []) === conditiona.value;
-				});
-				//if any were true, fire the delegator ON THIS ELEMENT
-				if (fire) api.getDelegator().trigger(delegator, el);
-			});
+(function(){
+	var check = function(el, conditional, delegator, api){
+		var targets = [];
+		//get the target of these delegators
+		if (conditional.targets){
+			targets = el.getElement(conditional.targets);
+		} else if (conditional.target && conditional.target != 'self'){
+			var target = el.getElement(conditional.target);
+			if (target) targets = new Elements([target]);
+		} else {
+			targets = new Elements([el]);
 		}
-	}
-});
+		if (targets.length == 0) api.fail('could not find targets for startup delegator: ', delegator, conditional.targets);
+		//check the targets for the conditionals
+		return targets.some(function(target){
+			if (conditional.property) return target.get(conditional.property) === conditional.value;
+			else if (conditional.method) return target[method].apply(element, conditional.arguments || []) === conditiona.value;
+		});
+	};
+	Behavior.addGlobalFilter('Startup', {
+		setup: function(el, api){
+			//get the delegators to set up
+			var delegators = api.get('delegators');
+			if (delegators){
+				Object.each(delegators, function(conditional, delegator){
+					//if any were true, fire the delegator ON THIS ELEMENT
+					if (check(el, conditional, delegator, api)) api.getDelegator().trigger(delegator, el);
+				});
+			}
+		}
+	});
+})();
